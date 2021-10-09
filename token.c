0e08d60c0bc20d8eb80531c891d68073b8d597e5
David Kedra <dejjvit@gmail.com>
2021-10-09 20:38:46 +0200

    Corrections in error and token files, key_words in type_ enum

--- token.c
+++ token.c
@@ -18,7 +18,8 @@
 
 
 //napr. pro token_t *token;  volat: token_create(&s, type, &token);
-int token_create(Istring *s, enum Token_type type, token_t **ref_token){
+int token_create(Istring *s, enum Token_type type, token_t **ref_token)
+{
     *ref_token = (token_t *) malloc(sizeof(token_t));
     
     //pro zjednoduseni prirazovani pres (*ref_token)-> na jen token->
@@ -41,21 +42,30 @@ int token_create(Istring *s, enum Token_type type, token_t **ref_token){
         {
             // konverze hodnoty na konstatni cislo typu int (melo by uz byt ve spravnem tvaru)
             token->value.int_val = strtol(s->value, NULL, 10);
+
+            // v get_token se ztraci ukazatel, pokud neni ulozen ve str_val tokenu
+            // je potreba dealokovat puvodni obsah istringu
+            string_Free(s);
         }
         else if(type == TYPE_NUMBER || type == TYPE_EXPONENT_NUMBER)
         {
             // konverze hodnoty na konstatni cislo typu C double
             token->value.float_val = strtod(s->value, NULL);
+
+            //hodnota tokenu neni str_val, musi se dealokovat puvodni retezec
+            string_Free(s);
         }
-        else if(type == TYPE_IDENTIFIER){
+        else if(type == TYPE_IDENTIFIER)
+        {
             // kontrola, jestli neni identifikator klicovym slovem jazyka
             token->type = check_key_words(s->value);
             
-            token->value.str_val = s->value;
+            // pokud je typem key word, nemusi se uz ukladat do hodnoty
+            if(token->type == TYPE_IDENTIFIER)
+                token->value.str_val = s->value;
         }
-        else{
+        else
             token->value.str_val = s->value;
-        }
     }
     else
     {
@@ -66,18 +76,16 @@ int token_create(Istring *s, enum Token_type type, token_t **ref_token){
     return 0;
 }
 
-void token_delete(token_t **token){
-    if(token)
+void token_delete(token_t **token)
+{
+    //uvolneni, pokud token neni ukazatel na NULLovy ukazatel na token 
+    if(token && *token)
     {
-        //pokud token neni ukazatel na NULL ukazatel na token 
-        if(*token)
-        {
-            // kdyz ma i neNULLovou hodnotu string, bude dealokovan
-            if((*token)->value.str_val)
-                free((*token)->value.str_val);
+        // kdyz ma i neNULLovou hodnotu string, bude dealokovan
+        if((*token)->value.str_val)
+            free((*token)->value.str_val);
 
         free(*token);
-        }
     }
     *token = NULL;
 }
@@ -85,53 +93,20 @@ void token_delete(token_t **token){
 
 int check_key_words(char *identif)
 {
-    if(!strcmp(identif, "do")){
-        return KW_DO;
-    }
-    else if(!strcmp(identif, "global")){
-        return KW_GLOBAL;
-    }
-    else if(!strcmp(identif, "number")){
-        return KW_NUMBER;
-    }
-    else if(!strcmp(identif, "else")){
-        return KW_ELSE;
-    }
-    else if(!strcmp(identif, "if")){
-        return KW_IF;
-    }
-    else if(!strcmp(identif, "require")){
-        return KW_REQUIRE;
-    }
-    else if(!strcmp(identif, "end")){
-        return KW_END;
-    }
-    else if(!strcmp(identif, "integer")){
-        return KW_INTEGER;
-    }
-    else if(!strcmp(identif, "return")){
-        return KW_RETURN;
-    }
-    else if(!strcmp(identif, "function")){
-        return KW_FUNCTION;
-    }
-    else if(!strcmp(identif, "local")){
-        return KW_LOCAL;
-    }
-    else if(!strcmp(identif, "string")){
-        return KW_STRING;
-    }
-    else if(!strcmp(identif, "nil")){
-        return KW_NIL;
-    }
-    else if(!strcmp(identif, "then")){
-        return KW_THEN;
-    }
-    else if(!strcmp(identif, "while")){
-        return KW_WHILE;
-    }
-    else
-    {
-        return TYPE_IDENTIFIER;
-    }
+    if(!strcmp(identif, "do"))              return TYPE_DO;
+    else if(!strcmp(identif, "global"))     return TYPE_GLOBAL;
+    else if(!strcmp(identif, "number"))     return TYPE_NUMBER;
+    else if(!strcmp(identif, "else"))       return TYPE_ELSE;
+    else if(!strcmp(identif, "if"))         return TYPE_IF;
+    else if(!strcmp(identif, "require"))    return TYPE_REQUIRE;
+    else if(!strcmp(identif, "end"))        return TYPE_END;
+    else if(!strcmp(identif, "integer"))    return TYPE_INTEGER;
+    else if(!strcmp(identif, "return"))     return TYPE_RETURN;
+    else if(!strcmp(identif, "function"))   return TYPE_FUNCTION;
+    else if(!strcmp(identif, "local"))      return TYPE_LOCAL;
+    else if(!strcmp(identif, "string"))     return TYPE_STRING;
+    else if(!strcmp(identif, "nil"))        return TYPE_NIL;
+    else if(!strcmp(identif, "then"))       return TYPE_THEN;
+    else if(!strcmp(identif, "while"))      return TYPE_WHILE;
+    else                                    return TYPE_IDENTIFIER;
 }
