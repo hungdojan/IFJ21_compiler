cf07e9a2ef7d99cc4ee625c6f32772e586de3589
David Kedra <dejjvit@gmail.com>
2021-10-09 12:36:19 +0200

    Adding a token struct and create, delete, check_key_words functions

--- token.h
+++ token.h
@@ -1,6 +1,6 @@
 /**
  * @file token.h
- * @brief Deklarace struktur token_t a token_array_t, jejich funkce a vycet typu tokenu
+ * @brief Deklarace struktur, vycet typu tokenu, token_t a jeho funkce
  *
  * @authors David Kedra        (xkedra00)
  *          Jaroslav Kvasnicka (xkvasn14)
@@ -44,12 +44,62 @@ enum Token_type {
     TYPE_UNDEFINED
 } type;
 
+enum Key_words
+{
+    KW_DO,
+    KW_GLOBAL,
+    KW_NUMBER,
+    KW_ELSE,
+    KW_IF,
+    KW_REQUIRE,
+    KW_END,
+    KW_INTEGER,
+    KW_RETURN,
+    KW_FUNCTION,
+    KW_LOCAL,
+    KW_STRING,
+    KW_NIL,
+    KW_THEN,
+    KW_WHILE
+} key_word;
+
+// mozne typy atributu tokenu, vyuzije se jeden z nich 
+typedef union
+{
+    int     int_val;
+    double  float_val;
+    char *  str_val;
+} token_value_t;
+
+// struktura pro token
+typedef struct{
+    enum Token_type type;
+    token_value_t value;
+} token_t;
+
+
 /**
- * @brief Vytvori novy token a ulozi ho do pole tokenu
+ * @brief Vytvori novy token a ulozi ho do ukazatele z argumentu
  *
- * @param s Hodnota retezce
+ * @param s Struktura lexemu s retezcem
  * @param type Typ tokenu
+ * @param ref_token Ukazatel na dynamicky token, kam se ulozi nove vytvoreny
  */
-void token_create(Istring *s, enum Token_type type);
+int token_create(Istring *s, enum Token_type type, token_t **ref_token);
+
+/**
+ * @brief Otestovani, zda identifikator neni klicovym slovem
+ * 
+ * @param identif Retezec obsahujici podeyrely identifikator
+ * @return Vraci konkretni typ klicoveho slova nebo vzchozi TYPE_IDENTIF 
+ */
+int check_key_words(char *identif);
+
+/**
+ * @brief Vymaze a dealokuje token a jeho atributy
+ *
+ * @param token Ukazatel na dynamicky token, ktery ma byt uvolnen
+ */
+void token_delete(token_t **token);
 
 #endif //_TOKEN_H_
