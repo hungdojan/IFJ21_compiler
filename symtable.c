4e8f5881282676069bb0789b29895ec429741ae1
David Kedra <dejjvit@gmail.com>
2021-11-09 21:49:11 +0100

    Binary search tree for Symtable

--- symtable.c
+++ symtable.c
@@ -10,5 +10,113 @@
  * Reseni projektu IFJ a IAL, FIT VUT 21/22
  */
 #include "symtable.h"
+#include "error.h"
+#include <stdlib.h>
+#include <string.h>
+
+int tree_init(node_ptr *tree)
+{
+    if(tree)
+    {
+        // vytvoreni specialni ROOT node, ktery se po insertu jakekoli stane tim novym uzlem
+        *tree = (node_ptr)malloc(sizeof(struct tree_node));
+        if(!(*tree))
+            return ERR_INTERNAL;
+
+        (*tree)->key = NULL;
+        (*tree)->left = NULL;
+        (*tree)->right = NULL;
+
+        (*tree)->type = ROOT; // staci odlisit pres NULLovy key
+    }
+    return 0;
+}
+
+int tree_insert(node_ptr *tree, char *key, item_type type)
+{
+    // ukazatel na root node, ani ona samotna nemuze byt null
+    if(!tree)
+        return ERR_INTERNAL;
+
+    // pokud nema byt nova node root uzlem, provedeme hledani dal
+    if(*tree && (*tree)->key != NULL)
+    {
+        while(*tree != NULL)
+        {
+            // hledani prislusne pozice noveho uzlu (tam kde je leaf NULL)
+            if(strcmp(key, (*tree)->key) < 0)
+                tree = &(*tree)->left;
+            else if(strcmp(key, (*tree)->key) > 0)
+                tree = &(*tree)->right;
+            
+            // klic existuje
+            else
+                return ERR_SEM_DEF;
+        }
+        // byl nalezen NULLovy leaf:
+
+        // alokace a prirazeni noveho node
+        *tree = (node_ptr)malloc(sizeof(struct tree_node));
+    }
+    // jestli ma byt uzel rootem, je node_ptr jiz alokovan z tree_init
+    
+    if(*tree)
+    {
+        (*tree)->left = NULL;
+        (*tree)->right = NULL;
+        (*tree)->type = type;
+        (*tree)->is_defined = 0;
+        (*tree)->param_num = 0; //todo
+        
+        (*tree)->key = (char *)malloc(sizeof(char)*(strlen(key)+1));
+        if(!(*tree)->key)
+            return ERR_INTERNAL;
+        strcpy((*tree)->key, key);
+    }
+    else    
+        return ERR_INTERNAL;
+        
+    return 0;
+}
+
+void tree_destroy(node_ptr *tree)
+{
+    if(tree && *tree)
+    {
+        if(*tree)
+        {
+            // je potreba uvolnit prave a leve uzly ze stejneho frame
+            tree_destroy(&(*tree)->left);
+            tree_destroy(&(*tree)->right);
+        
+            if((*tree)->key)
+                free((*tree)->key);
+            free(*tree);
+
+            *tree = NULL;
+        }
+    }
+}
+
+node_ptr tree_search(node_ptr tree, char *key)
+{
+    if(!tree)
+        return NULL;
+
+    while(tree != NULL)
+    {
+        // hledani ve frame funkce
+        if(strcmp(key, tree->key) < 0)
+            tree = tree->left;
+        else if(strcmp(key, tree->key) > 0)
+            tree = tree->right;
+        else
+            // klic nalezen
+            return tree;
+    }
+
+    // identifikator nebyl nalezen v local_frame toho stromu
+    return NULL;
+}
 
 /* symtable.c */
--- symtable.h
+++ symtable.h
@@ -12,7 +12,63 @@
 #ifndef _SYMTABLE_H_
 #define _SYMTABLE_H_
 
-#include "bintree.h"
+// vycet moznych typu uzlu symtablu
+typedef enum{
+    VAR,
+    FUNC,
+
+    ROOT,
+} item_type;
+
+
+// node_ptr je ukazatel na 'struct tree_node' uzel, ktery je sam stromem
+typedef struct tree_node
+{
+    char *key;                      /// Nazev identifikatoru
+    item_type type;                 /// Typ identifikatoru
+    
+    int is_defined;                 /// Bool, jestli byl definovan
+    int param_num;                  /// Pocet parametru funkce 
+
+    struct tree_node *left;         /// Ukazatel na levy podstrom (klice mensi)
+    struct tree_node *right;        /// Ukazatel na pravy podstrom (klice vetsi)
+
+} *node_ptr;
+
+/**
+ * @brief Inicializace stromu (frame), ktera vytvori specialni ROOT node s nealokovanym klicem NULL. 
+ *        Bude-li strom uzivan i po tree_destroy, je potreba nejdrive volat tuto funkci
+ *
+ * @param tree Ukazatel na strom, ve kterem ma byt vytvoren pocatecni ROOT uzel
+ * @return Error kod
+*/
+int tree_init(node_ptr *tree);
+
+/**
+ * @brief Vytvoreni a vlozeni noveho uzlu do stromu
+ *
+ * @param tree Ukazatel na strom (frame), do ktereho se ulozi novy uzel (identifikator)
+ * @param key String klic pridavaneho uzlu
+ * @param type Typ noveho uzlu
+ * @return Error kod
+*/
+int tree_insert(node_ptr *tree, char *key, item_type type);
+
+/**
+ * @brief Dealokace kompletniho stromu a bocnich podstromu
+ *
+ * @param tree Ukazatel na strom, ktery bude uvolnen a smazan
+*/
+void tree_destroy(node_ptr *tree);
+
+/**
+ * @brief Hledani uzlu/identifikatoru s konkretnim klicem
+ *
+ * @param tree Strom, ve kterem probiha hledani klice
+ * @param key Hledany klic uzlu
+ * @return Ukazatel na nalezeny uzel
+*/
+node_ptr tree_search(node_ptr tree, char *key);
 
 #endif
 /* symtable.h */
