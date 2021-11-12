0c961ce02dbdb84f9443ffef8cb8e49b9a4f589a
Petr Kolarik <peta.kolarik@seznam.cz>
2021-11-12 20:52:52 +0100

    new generator.c new generator.h

---
+++ generator.c
@@ -0,0 +1,35 @@
+/**
+ * @file generator.c
+ * @brief Definice retezcovych funkci
+ *
+ * @authors Hung Do            (xdohun00)
+ *          David Kedra        (xkedra00)
+ *          Petr Kolarik       (xkolar79)
+ *          Jaroslav Kvasnicka (xkvasn14)
+ *
+ * Reseni projektu IFJ a IAL, FIT VUT 21/22
+ */
+
+#include "generator.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+code_t* gen_code(enum ins_type, char *, char *, char *)
+{
+    return NULL;
+}
+void queue_flush(queue_t *queue);
+{
+    for(code_t item = queue->first; item != NULL; item = item->next)
+    {
+
+    }
+}
+void queue_init(queue_t *);
+{}
+void queue_destroy(queue_t *);
+{}
+int queue_add(queue_t *);
+{
+    return -1;
+}
